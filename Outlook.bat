0677f4c02ebe30138798d65e641c975ff9d47343
SempaiRock <125228431+SempaiRock@users.noreply.github.com>
2024-10-11 12:38:06 +0600

    Update Outlook.bat
    Signed-off-by: SempaiRock <125228431+SempaiRock@users.noreply.github.com>

--- Outlook.bat
+++ Outlook.bat
@@ -1,21 +1,31 @@
 @echo off
 :check_Permissions
-    echo Administrative permissions required. Detecting permissions...
-    
-    net session >nul 2>&1
-    if %errorLevel% == 0 (
-        echo Success: Administrative permissions confirmed.
-    ) else (
-        echo Failure: You need to run this as Admin!!!
-		pause
-		goto konec
-    )
+echo Administrative permissions required. Detecting permissions...
 
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
-if %OS%==64BIT copy AppxManifest.xml %systemdrive%\users
-if %OS%==32BIT copy AppxManifestx86.xml %systemdrive%\users\AppxManifest.xml
-powershell "New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1 -Force" >NUL 2>NUL
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Success: Administrative permissions confirmed.
+) else (
+    echo Failure: You need to run this as Admin!!!
+    pause
+    goto konec
+)
+
+echo Copying AppxManifest.xml to C:\Users\
+copy "%~dp0AppxManifest.xml" "C:\Users\" >nul
+if %errorLevel% neq 0 (
+    echo Error: Failed to copy the file. Check the file path and try again.
+    pause
+    goto konec
+)
+
+echo Modifying registry to allow development mode...
+powershell "New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1 -Force" >nul 2>nul
+
+echo Removing Outlook appx package...
 powershell "get-appxpackage -allusers Microsoft.OutlookForWindows | Remove-AppxPackage -allusers"
-powershell add-appxpackage -register "%systemdrive%\users\AppxManifest.xml"
+
+echo Registering AppxManifest.xml...
+powershell add-appxpackage -register "C:\Users\AppxManifest.xml"
 
 :konec
